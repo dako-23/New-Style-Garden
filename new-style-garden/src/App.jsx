8e18f2a751591f05a643359f7c6f7675dcede66c
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-04-07 17:42:48 +0300

    Added react router & first routes

--- new-style-garden/src/App.jsx
+++ new-style-garden/src/App.jsx
@@ -1,12 +1,29 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import ScrollToTop from "../helpers/scrollToTop.js";
+import ToastConfig from "./components/notifications/ToastConfig.jsx";
+import Spinner from "./components/loading-spinner/Spinner.jsx";
+import About from "./components/about/About.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
 
 function App() {
 
   return (
-    <>
-      <h1 className="bg-lime-700">HELLOO</h1>
-    </>
-  )
+    <Router>
+      <Suspense fallback={<Spinner />}>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/about" element={<About />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+          {/* <Route path="/terms-and-conditions" element={<TermsAndConditions />} /> */}
+        </Routes>
+        <ToastConfig />
+        {/* <Footer /> */}
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App
