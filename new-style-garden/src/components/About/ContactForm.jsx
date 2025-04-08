9cc88307d03cbbe5594e789fccc398f253d836c7
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-04-08 13:04:55 +0300

    Added input field for phone number

--- new-style-garden/src/components/About/ContactForm.jsx
+++ new-style-garden/src/components/About/ContactForm.jsx
@@ -1,6 +1,7 @@
-import { useActionState } from "react";
-import emailjs from "@emailjs/browser";
-import { FaEnvelope, FaUser, FaCommentDots } from "react-icons/fa";
+import { useActionState } from 'react';
+import emailjs from '@emailjs/browser';
+import { FaEnvelope, FaUser, FaCommentDots, FaPhone } from 'react-icons/fa';
+import Spinner from '../loading-spinner/Spinner.jsx';
 
 export default function ContactForm({ onSuccess, onError }) {
 
@@ -9,85 +10,108 @@ export default function ContactForm({ onSuccess, onError }) {
 
         try {
             await emailjs.send(
-                "service_finuo7b",
-                "template_9s5cp6p",
+                'service_finuo7b',
+                'template_9s5cp6p',
                 {
                     name: values.name,
                     email: values.email,
                     message: values.message,
                 },
-                "zhdhBPBbfAsc09QO1"
+                'zhdhBPBbfAsc09QO1'
             );
             values.title = '',
                 values.name = '',
                 values.email = '',
-                values.message = ''
+                values.message = '',
+                values.phone = '',
 
-            onSuccess("Email sent successfully!");
+                onSuccess('Email sent successfully!');
+
         } catch (err) {
-            onError("Error sending email.");
+            onError('Error sending email.');
         }
 
         return values;
     };
 
     const [values, contactAction, isPending] = useActionState(handleSubmitContact, {
-        name: "",
-        email: "",
-        message: "",
+        name: '',
+        email: '',
+        message: '',
+        phone: '',
     });
 
     return (
-        <form
-            action={contactAction}
-            className="bg-white p-6 shadow-lg rounded-xl border border-gray-200"
-        >
-            <h3 className="text-2xl font-semibold text-gray-700 mb-4 flex items-center gap-2">
-                <FaEnvelope className="text-gray-700" /> Изпрати запитване
-            </h3>
-
-            <div className="space-y-4">
-                
-                <div className="flex items-center  p-3 rounded-lg bg-gray-100">
-                    <FaUser className="text-gray-500 mr-2" />
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Вашето име"
-                        className="w-full bg-transparent outline-none"
-                        defaultValue={values.name}
-                    />
-                </div>
-
-                <div className="flex items-center  p-3 rounded-lg bg-gray-100">
-                    <FaEnvelope className="text-gray-500 mr-2" />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Имейл"
-                        className="w-full bg-transparent outline-none"
-                        defaultValue={values.email}
-                    />
-                </div>
-
-                <div className="flex items-start  p-3 rounded-lg bg-gray-100">
-                    <FaCommentDots className="text-gray-500 mr-2 mt-1" />
-                    <textarea
-                        name="message"
-                        placeholder="Вашето запитване..."
-                        className="w-full bg-transparent outline-none resize-none h-24"
-                        defaultValue={values.message}
-                    />
-                </div>
-
-                <button
-                    type="submit"
-                    className="w-full py-3 bg-gray-700 text-white font-semibold rounded-lg shadow-md hover:bg-gray-600 transition cursor-pointer"
-                    disabled={isPending}
+        <>
+            {isPending
+                ?
+                <Spinner />
+                :
+                <form
+                    action={contactAction}
+                    className="bg-white p-6 shadow-lg rounded-xl border border-gray-200"
                 >
-                    Изпрати запитване
-                </button>
-            </div>
-        </form>
+                    <h3 className="text-2xl font-semibold text-gray-700 mb-4 flex items-center gap-2">
+                        <FaEnvelope className="text-gray-700" /> Изпрати запитване
+                    </h3>
+
+                    <div className="space-y-4">
+
+                        <div className="flex items-center  p-3 rounded-lg bg-gray-100">
+                            <FaUser className="text-gray-500 mr-2" />
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Вашето име"
+                                className="w-full bg-transparent outline-none"
+                                defaultValue={values.name}
+                            />
+                        </div>
+
+                        <div className="flex items-center  p-3 rounded-lg bg-gray-100">
+                            <FaEnvelope className="text-gray-500 mr-2" />
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Имейл"
+                                className="w-full bg-transparent outline-none"
+                                defaultValue={values.email}
+                            />
+                        </div>
+
+                        <div className="flex items-start  p-3 rounded-lg bg-gray-100">
+                            <FaCommentDots className="text-gray-500 mr-2 mt-1" />
+                            <textarea
+                                name="message"
+                                placeholder="Вашето запитване..."
+                                className="w-full bg-transparent outline-none resize-none h-24"
+                                defaultValue={values.message}
+                            />
+                        </div>
+
+                        <div className="flex items-center p-3 rounded-lg bg-gray-100">
+                            <FaPhone className="text-gray-500 mr-2" />
+                            <input
+                                type="tel"
+                                name="phone"
+                                placeholder="Телефон"
+                                className="w-full bg-transparent outline-none"
+                                defaultValue={values.phone}
+                            />
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full py-3 bg-gray-700 text-white font-semibold rounded-lg shadow-md hover:bg-gray-600 transition cursor-pointer"
+                            disabled={isPending}
+                        >
+                            Изпрати запитване
+                        </button>
+                    </div>
+                </form>
+            }
+
+        </>
     );
+
 }
