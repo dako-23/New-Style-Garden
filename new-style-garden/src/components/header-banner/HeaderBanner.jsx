82ff6df57ab6bf1fa8ed393989c5f252048002df
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-04-09 12:38:57 +0300

    transition duration change

--- new-style-garden/src/components/header-banner/HeaderBanner.jsx
+++ new-style-garden/src/components/header-banner/HeaderBanner.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 export default function HeaderBanner({ title }) {
     return (
         <div
-            className="relative w-full h-80 flex items-center justify-center text-center bg-cover bg-center"
+            className="relative w-full h-70 flex items-center justify-center text-center bg-cover bg-center"
             style={{ backgroundImage: 'url("/page-pattern3.jpg")' }}>
             <div className="absolute inset-0 bg-opacity-50 z-0"></div>
 
@@ -12,7 +12,7 @@ export default function HeaderBanner({ title }) {
                 <motion.h1
                     initial={{ opacity: 0, x: -60 }}
                     animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.3, ease: "easeOut"}}
+                    transition={{ duration: 0.4, ease: "easeOut"}}
                     className="text-5xl font-bold mt-18"
                 >
                     {title}
