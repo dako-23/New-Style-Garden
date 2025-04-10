b2f1e881919b689e5e912955aaee9443c18f02f5
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-04-10 17:58:58 +0300

    Added services irrigation component

---
+++ new-style-garden/src/components/services-page/ServicesIrrigation.jsx
@@ -0,0 +1,13 @@
+import HeaderBanner from "../header-banner/HeaderBanner.jsx";
+import ServiceTimeline from "./ServiceTimeline.jsx";
+
+
+
+
+export default function ServicesIrrigation() {
+    return (<>
+        <HeaderBanner title={'Поливни системи'} />
+        <ServiceTimeline />
+    </>
+    );
+}
