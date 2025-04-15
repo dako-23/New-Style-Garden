a9a9a8db51de4f21232c7c173cd8c6c9aeecb17a
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-04-15 14:29:46 +0300

    Added toast custom hook

---
+++ new-style-garden/src/hooks/useToast.js
@@ -0,0 +1,10 @@
+import { toast } from 'react-toastify';
+
+export const useToast = () => {
+    return {
+        success: (msg) => toast.success(msg),
+        error: (msg) => toast.error(msg),
+        info: (msg) => toast.info(msg, { autoClose: 500 }),
+        warn: (msg) => toast.warn(msg),
+    };
+};
